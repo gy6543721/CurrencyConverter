a8602ca1626e52d292767cda773cc3d4db50454e
gy6543721 <gy6543721@yahoo.com.tw>
2023-01-16 18:11:19 +0900

    Update Icon & Gradle

---
+++ app/src/main/java/levilin/currencyconverter/CurrencyConverterApplication.kt
@@ -0,0 +1,8 @@
+package levilin.currencyconverter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyConverterApplication: Application() {
+}
